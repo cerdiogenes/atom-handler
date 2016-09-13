154b27cb34d408e318e9a676f087c2b105bd090b
Kadu Diógenes <kadu@fnix.com.br>
2016-09-13 20:13:26 -0300

    generate atom related files

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 mkdir -p $HOME/bin
-cat > $HOME/bin/subl-handler <<EOF
+cat > $HOME/bin/atom-handler <<EOF
 #!/bin/bash
-FILE=\$1
-FILE=\${FILE/subl\:\/\//}
-subl \$FILE
+request=\${1:23} # Delete first 23 characters == atom://open?url=file://
+request=\$(echo \$request | echo -e "\$(sed 's/+/ /g; s/%/\\\x/g')") # Decode URL
+request=\${request/&line=/:}   # Replace &line= with :
+request=\${request/&column=/:} # Replace &column= with :
+atom \$request # Launch Atom
 EOF
-chmod u+x $HOME/bin/subl-handler
+chmod u+x $HOME/bin/atom-handler
 
 mkdir -p $HOME/.local/share/applications/
-cat > $HOME/.local/share/applications/subl-handler.desktop <<EOF
+cat > $HOME/.local/share/applications/atom-handler.desktop <<EOF
 [Desktop Entry]
-Name=subl-handler
-Comment=Sublime Text URI scheme subl:// handler
-Exec=$HOME/bin/subl-handler %u
+Name=atom-handler
+Comment=Atom URI scheme atom:// handler
+Exec=$HOME/bin/atom-handler %u
 Terminal=0
 Type=Application
 X-MultipleArgs=True
-MimeType=x-scheme-handler/subl
+MimeType=x-scheme-handler/atom
 Encoding=UTF-8
 Categories=Application;
 EOF
 
 update-desktop-database $HOME/.local/share/applications/
-xdg-mime default subl-handler.desktop x-scheme-handler/subl
+xdg-mime default atom-handler.desktop x-scheme-handler/atom
